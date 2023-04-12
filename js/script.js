3503d67e2441e9d7944288f8a375550b75da787f
Nikita(Smail96251) <hveshchenko@gmail.com>
2023-04-12 20:49:21 +0300

    feat: add functions mouse up and mouse move add background color element

--- js/script.js
+++ js/script.js
@@ -3,6 +3,22 @@ const sizeSlider = document.querySelector("#sizeSlider");
 const containerGrid = document.querySelector("#grid");
 
 let elementsValue = 16;
+let is_active;
+
+window.onmousedown = function () {
+  is_active = true;
+};
+
+window.onmouseup = function () {
+  is_active = false;
+};
+
+function colorElement(e) {
+  if (is_active) {
+    e.target.style.backgroundColor = "black";
+    e.target.style.border = "black";
+  }
+}
 
 function clearGridContainer() {
   containerGrid.innerHTML = "";
@@ -11,12 +27,14 @@ function clearGridContainer() {
 function createGridElement() {
   clearGridContainer();
 
-  grid.style.gridTemplateColumns = `repeat(${elementsValue}, 1fr)`;
-  grid.style.gridTemplateRows = `repeat(${elementsValue}, 1fr)`;
+  containerGrid.style.gridTemplateColumns = `repeat(${elementsValue}, 1fr)`;
+  containerGrid.style.gridTemplateRows = `repeat(${elementsValue}, 1fr)`;
 
   for (let i = 0; i < elementsValue * elementsValue; i++) {
     const element = document.createElement("div");
     element.classList.add("grid-element");
+    element.addEventListener("mousemove", colorElement);
+    element.addEventListener("mouseup", colorElement);
     containerGrid.appendChild(element);
   }
 }
@@ -28,3 +46,5 @@ function updateSizeValue(e) {
 
 sizeSlider.addEventListener("mousemove", updateSizeValue);
 sizeSlider.addEventListener("mouseup", createGridElement);
+
+createGridElement();
